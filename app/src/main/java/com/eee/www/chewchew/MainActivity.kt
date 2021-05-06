0ed291b8d4a2a377eb770e0d2a544883f1b2d880
Wanjuuuuu <makarong5798@gmail.com>
2021-05-07 00:11:43 +0900

    CountPicker와 ViewModel 연결하기

--- app/src/main/java/com/eee/www/chewchew/MainActivity.kt
+++ app/src/main/java/com/eee/www/chewchew/MainActivity.kt
@@ -2,10 +2,26 @@ package com.eee.www.chewchew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.eee.www.chewchew.viewmodels.PreferenceViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: PreferenceViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initCountPicker()
+    }
+
+    private fun initCountPicker() {
+        countPicker.value = viewModel.fingerCount.value!!
+        countPicker.setOnValueChangedListener { _, _, newVal ->
+            viewModel.setFingerSelectionCount(
+                newVal
+            )
+        }
     }
 }
