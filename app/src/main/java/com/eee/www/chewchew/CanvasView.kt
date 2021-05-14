01d03a02f2edf83d895e27b37ef9a69465cb7346
eunwoop <eeunwoop@gmail.com>
2021-05-14 18:33:01 +0900

    원 대신 카드모양으로 리소스 변경
    그런데 문제는! 배경이 뭔가 너무 커서 이상하다

--- app/src/main/java/com/eee/www/chewchew/CanvasView.kt
+++ app/src/main/java/com/eee/www/chewchew/CanvasView.kt
@@ -9,23 +9,19 @@ import android.util.Log
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
-import com.eee.www.chewchew.CanvasView.CanvasViewConstants.CIRCLE_SIZE
 import com.eee.www.chewchew.CanvasView.CanvasViewConstants.MAX_TOUCH
 import com.eee.www.chewchew.CanvasView.CanvasViewConstants.MESSAGE_PICK
-import com.eee.www.chewchew.CanvasView.CanvasViewConstants.SELECTED_CIRCLE_SIZE
 import com.eee.www.chewchew.CanvasView.CanvasViewConstants.TAG
 import com.eee.www.chewchew.CanvasView.CanvasViewConstants.WAITING_TIME
-import java.lang.IllegalArgumentException
+
 
 class CanvasView : View {
     object CanvasViewConstants {
         const val TAG = "CanvasView"
         const val MAX_TOUCH = 10
-        const val CIRCLE_SIZE = 50
-        const val SELECTED_CIRCLE_SIZE = 100
         const val WAITING_TIME = 3000L
-
         const val MESSAGE_PICK = 0
     }
 
@@ -43,15 +39,30 @@ class CanvasView : View {
         }
         return@Callback false
     });
+    private var mCardBitmap = mutableListOf<Bitmap?>()
+    private var mBigCardBitmap = mutableListOf<Bitmap?>()
 
     init {
         mColorList = (context?.let { ColorLoader.getInstance(it).getColorList() }
                 as ArrayList<Int>?)!!
+
+        for (i in 0..9) {
+            val bitmap = mContext?.let {
+                getBitmapFromVectorDrawable(it, R.drawable.ic_credit_card, mColorList[i])
+            }
+            mCardBitmap.add(bitmap)
+            mBigCardBitmap.add(Bitmap.createScaledBitmap(bitmap!!,
+                bitmap!!.width*2, bitmap!!.height*2, false))
+        }
     }
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
     override fun onDraw(canvas: Canvas) {
         val paint = Paint()
@@ -63,8 +74,9 @@ class CanvasView : View {
             val point = mTouchPointMap[index];
             paint.color = mColorList[index];
             if (point != null) {
-                canvas.drawCircle(point.x, point.y,
-                        dpToPx(mContext, CIRCLE_SIZE.toFloat()), paint)
+                val x = point.x- ((mCardBitmap[index]?.width ?: 0))/2
+                val y = point.y- ((mCardBitmap[index]?.height ?: 0))/2
+                mCardBitmap[index]?.let { canvas.drawBitmap(it, x, y, paint) }
             }
         }
 
@@ -73,8 +85,9 @@ class CanvasView : View {
                 val point = mTouchPointMap[it]
                 paint.color = mColorList[it]
                 if (point != null) {
-                    canvas.drawCircle(point.x, point.y,
-                            dpToPx(mContext, SELECTED_CIRCLE_SIZE.toFloat()), paint)
+                    val x = point.x- ((mBigCardBitmap[it]?.width ?: 0))/2
+                    val y = point.y- ((mBigCardBitmap[it]?.height ?: 0))/2
+                    mBigCardBitmap[it]?.let { its -> canvas.drawBitmap(its, x, y, paint) }
                 }
             }
         }
@@ -107,7 +120,7 @@ class CanvasView : View {
                     invalidate()
                 }
             }
-        } catch (e : IllegalArgumentException) {
+        } catch (e: IllegalArgumentException) {
             Log.e(TAG, "IllegalArgumentException happened in onTouch$e")
         }
         return true
@@ -137,7 +150,7 @@ class CanvasView : View {
         }
     }
 
-    private fun removeCoord(event: MotionEvent,  pointerIndex: Int, pointerId: Int) {
+    private fun removeCoord(event: MotionEvent, pointerIndex: Int, pointerId: Int) {
         mTouchPointMap.remove(pointerId)
         if (mTouchPointMap.isEmpty()) {
             mSelected.clear()
@@ -145,7 +158,7 @@ class CanvasView : View {
         }
     }
 
-    private fun pickN(n:Int) {
+    private fun pickN(n: Int) {
         if (mTouchPointMap.isEmpty()) {
             return
         }
@@ -180,9 +193,24 @@ class CanvasView : View {
 
     private fun dpToPx(context:Context?, dp:Float) : Float {
         if (context != null) {
-            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                    dp, context.resources.displayMetrics)
+            return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp, context.resources.displayMetrics
+            )
         };
         return dp;
+    }
+
+    private fun getBitmapFromVectorDrawable(context: Context, drawableId:Int, color:Int): Bitmap? {
+        val drawable = ContextCompat.getDrawable(context, drawableId) ?: return null;
+        drawable.setColorFilter(color, PorterDuff.Mode.OVERLAY)
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888);
+
+        val canvas = Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.width, canvas.height);
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 }
