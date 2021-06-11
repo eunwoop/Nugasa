47548e891cc68c56c517db73de9a4ef5ca7cd643
eunwoop <eeunwoop@gmail.com>
2021-06-12 00:13:13 +0900

    NumberPicker->Spinner
    동작추가

--- app/src/main/java/com/eee/www/chewchew/MainActivity.kt
+++ app/src/main/java/com/eee/www/chewchew/MainActivity.kt
@@ -2,6 +2,8 @@ package com.eee.www.chewchew
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initCanvasView()
-        initCountPicker()
+        initCountPicker(viewModel.pickList)
         initMenuSpinner()
     }
 
@@ -42,21 +44,71 @@ class MainActivity : AppCompatActivity() {
             Observer { fingerCount -> canvasView.fingerCount = fingerCount })
     }
 
-    private fun initCountPicker() {
-        countPicker.value.apply { viewModel.fingerCount.value }
-        countPicker.setOnValueChangedListener { _, _, newVal ->
-            viewModel.setFingerSelectionCount(
-                newVal
-            )
-        }
-    }
-
     private fun initMenuSpinner() {
         ArrayAdapter.createFromResource(this, R.array.menu_array,
-                android.R.layout.simple_spinner_item
+            android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             menuSpinner.adapter = adapter
+        }
+
+        menuSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position) {
+                    // pick
+                    0-> {
+                        countSpinner.isEnabled = true
+                        countSpinner.visibility = View.VISIBLE
+                        initCountPicker(viewModel.pickList)
+                    }
+                    // team
+                    1-> {
+                        countSpinner.isEnabled = true
+                        countSpinner.visibility = View.VISIBLE
+                        initCountPicker(viewModel.teamList)
+                    }
+                    // rank
+                    2-> {
+                        countSpinner.isEnabled = false
+                        countSpinner.visibility = View.GONE
+                    }
+                }
+                viewModel.setFingerSelectionCount(viewModel.pickList[position])
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+    }
+
+    private fun initCountPicker(countList: MutableList<Int>) {
+        val arrayAdapter = ArrayAdapter<Int>(this, android.R.layout.simple_spinner_item,
+            countList)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        countSpinner.adapter = arrayAdapter
+
+        // set initial value -> not sure needed.
+//        if (viewModel.fingerCount.value != null) viewModel.fingerCount.value else 0.let {
+//            countSpinner.setSelection(countList[it])
+//        }
+
+        countSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                viewModel.setFingerSelectionCount(countList[position])
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
         }
     }
 
