f87401aabe701e9f4e81f33b5285e032f98aa105
eunwoop <eeunwoop@gmail.com>
2022-11-06 21:36:05 +0900

    Clean code for IntroActivity
    * remove unnecessary member variable -> local variable
    * Apply viewBinding
    * remove make fullscreen

--- app/src/main/java/com/eee/www/nugasa/ui/IntroActivity.kt
+++ app/src/main/java/com/eee/www/nugasa/ui/IntroActivity.kt
@@ -4,97 +4,81 @@ import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Html
-import android.view.*
-import android.widget.Button
-import android.widget.LinearLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
-
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
-import androidx.viewpager.widget.ViewPager
 import com.eee.www.nugasa.R
-
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity  : AppCompatActivity() {
-    private var viewPager: ViewPager? = null
-    private var myViewPagerAdapter: MyViewPagerAdapter? = null
-    private var dotsLayout: LinearLayout? = null
-    private lateinit var dots: Array<TextView?>
-    private lateinit var layouts: IntArray
-    private var btnSkip: Button? = null
-    private  var btnNext:Button? = null
-    private var context = this as Context
+    private val layouts: IntArray = intArrayOf(
+        R.layout.intro_slide1,
+        R.layout.intro_slide2,
+        R.layout.intro_slide3
+    )
+    private val context = this as Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // TODO: use data binding
         setContentView(R.layout.activity_intro)
-
-        //TODO: remove it
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-
-        viewPager = findViewById<View>(R.id.view_pager) as ViewPager
-        dotsLayout = findViewById<View>(R.id.layoutDots) as LinearLayout
-        btnSkip = findViewById<View>(R.id.btn_skip) as Button
-        btnNext = findViewById<View>(R.id.btn_next) as Button
-
-        layouts = intArrayOf(
-            R.layout.intro_slide1,
-            R.layout.intro_slide2,
-            R.layout.intro_slide3
-        )
 
         addBottomDots(0)
 
         // making notification bar transparent
         changeStatusBarColor()
-        myViewPagerAdapter = MyViewPagerAdapter()
-        viewPager?.adapter = myViewPagerAdapter
-        viewPager?.addOnPageChangeListener(object : OnPageChangeListener {
-            override fun onPageSelected(position: Int) {
-                addBottomDots(position)
-                if (position == layouts.size - 1) {
-                    btnNext?.text = getString(R.string.start)
-                    btnSkip?.visibility = View.GONE
-                } else {
-                    btnNext?.text = getString(R.string.next)
-                    btnSkip?.visibility = View.VISIBLE
-                }
-            }
-            override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {}
-            override fun onPageScrollStateChanged(arg0: Int) {}
-        })
-        btnSkip?.setOnClickListener { finish() }
-        btnNext?.setOnClickListener {
+
+        initViewPager()
+        btnSkip.setOnClickListener { finish() }
+        btnNext.setOnClickListener {
             val current = getItem(+1)
             if (current < layouts.size) {
-                viewPager?.currentItem = current
+                viewPager.currentItem = current
             } else {
                 finish()
             }
         }
     }
 
+    private fun initViewPager() {
+        viewPager.adapter = IntroViewPagerAdapter()
+        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                addBottomDots(position)
+                if (position == layouts.size - 1) {
+                    btnNext.text = getString(R.string.start)
+                    btnSkip.visibility = View.GONE
+                } else {
+                    btnNext.text = getString(R.string.next)
+                    btnSkip.visibility = View.VISIBLE
+                }
+            }
+            override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {}
+            override fun onPageScrollStateChanged(arg0: Int) {}
+        })
+    }
+
     private fun addBottomDots(currentPage: Int) {
-        dots = arrayOfNulls(layouts.size)
+        val dots: Array<TextView?> = arrayOfNulls(layouts.size)
         val colorsActive = resources.getColor(R.color.dot_active)
         val colorsInactive = resources.getColor(R.color.dot_inactive)
-        dotsLayout?.removeAllViews()
+        dotsLayout.removeAllViews()
         for (i in dots.indices) {
             dots[i] = TextView(this)
             dots[i]?.text = Html.fromHtml("&#8226;")
             dots[i]?.textSize = 35f
             dots[i]?.setTextColor(colorsInactive)
-            dotsLayout?.addView(dots[i])
+            dotsLayout.addView(dots[i])
         }
         if (dots.isNotEmpty()) dots[currentPage]?.setTextColor(colorsActive)
     }
 
     private fun getItem(i: Int): Int {
-        return viewPager!!.currentItem + i
+        return viewPager.currentItem + i
     }
 
     private fun changeStatusBarColor() {
@@ -104,13 +88,12 @@ class IntroActivity  : AppCompatActivity() {
         }
     }
 
-    inner class MyViewPagerAdapter : PagerAdapter() {
-        private var layoutInflater: LayoutInflater? = null
+    inner class IntroViewPagerAdapter : PagerAdapter() {
+        private var layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val view: View? = layoutInflater?.inflate(layouts.get(position), container, false)
+            val view = layoutInflater.inflate(layouts[position], container, false)
             container.addView(view)
-            return view as Any
+            return view
         }
 
         override fun getCount(): Int {
