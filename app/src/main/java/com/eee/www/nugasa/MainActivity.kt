ecc85b109517c199bb39da94a8f5f4390611012c
eunwoop <eeunwoop@gmail.com>
2022-11-12 01:06:43 +0900

    Apply databinding to IntroActivity

--- app/src/main/java/com/eee/www/nugasa/MainActivity.kt
+++ app/src/main/java/com/eee/www/nugasa/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.eee.www.nugasa
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
--- app/src/main/java/com/eee/www/nugasa/ui/IntroActivity.kt
+++ app/src/main/java/com/eee/www/nugasa/ui/IntroActivity.kt
@@ -10,75 +10,47 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.eee.www.nugasa.R
-import kotlinx.android.synthetic.main.activity_intro.*
+import com.eee.www.nugasa.databinding.ActivityIntroBinding
 
-class IntroActivity  : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     private val layouts: IntArray = intArrayOf(
         R.layout.intro_slide1,
         R.layout.intro_slide2,
         R.layout.intro_slide3
     )
     private val context = this as Context
+    private lateinit var binding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+        binding.activity = this
 
         addBottomDots(0)
 
         // making notification bar transparent
         changeStatusBarColor()
-
         initViewPager()
-        btnSkip.setOnClickListener { finish() }
-        btnNext.setOnClickListener {
-            val current = getItem(+1)
-            if (current < layouts.size) {
-                viewPager.currentItem = current
-            } else {
-                finish()
-            }
-        }
-    }
-
-    private fun initViewPager() {
-        viewPager.adapter = IntroViewPagerAdapter()
-        viewPager.addOnPageChangeListener(object : OnPageChangeListener {
-            override fun onPageSelected(position: Int) {
-                addBottomDots(position)
-                if (position == layouts.size - 1) {
-                    btnNext.text = getString(R.string.start)
-                    btnSkip.visibility = View.GONE
-                } else {
-                    btnNext.text = getString(R.string.next)
-                    btnSkip.visibility = View.VISIBLE
-                }
-            }
-            override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {}
-            override fun onPageScrollStateChanged(arg0: Int) {}
-        })
     }
 
     private fun addBottomDots(currentPage: Int) {
         val dots: Array<TextView?> = arrayOfNulls(layouts.size)
         val colorsActive = resources.getColor(R.color.dot_active)
         val colorsInactive = resources.getColor(R.color.dot_inactive)
-        dotsLayout.removeAllViews()
+
+        binding.dotsLayout.removeAllViews()
         for (i in dots.indices) {
             dots[i] = TextView(this)
             dots[i]?.text = Html.fromHtml("&#8226;")
             dots[i]?.textSize = 35f
             dots[i]?.setTextColor(colorsInactive)
-            dotsLayout.addView(dots[i])
+            binding.dotsLayout.addView(dots[i])
         }
         if (dots.isNotEmpty()) dots[currentPage]?.setTextColor(colorsActive)
-    }
-
-    private fun getItem(i: Int): Int {
-        return viewPager.currentItem + i
     }
 
     private fun changeStatusBarColor() {
@@ -86,6 +58,41 @@ class IntroActivity  : AppCompatActivity() {
             addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             statusBarColor = Color.TRANSPARENT
         }
+    }
+
+    private fun initViewPager() {
+        binding.viewPager.adapter = IntroViewPagerAdapter()
+        binding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                addBottomDots(position)
+                if (position == layouts.size - 1) {
+                    binding.btnNext.text = getString(R.string.start)
+                    binding.btnSkip.visibility = View.GONE
+                } else {
+                    binding.btnNext.text = getString(R.string.next)
+                    binding.btnSkip.visibility = View.VISIBLE
+                }
+            }
+            override fun onPageScrolled(arg0: Int, arg1: Float, arg2: Int) {}
+            override fun onPageScrollStateChanged(arg0: Int) {}
+        })
+    }
+
+    fun onNextButtonClicked() {
+        val current = getItem(+1)
+        if (current < layouts.size) {
+            binding.viewPager.currentItem = current
+        } else {
+            finish()
+        }
+    }
+
+    private fun getItem(i: Int): Int {
+        return binding.viewPager.currentItem + i
+    }
+
+    fun onSkipButtonClicked() {
+        finish()
     }
 
     inner class IntroViewPagerAdapter : PagerAdapter() {
