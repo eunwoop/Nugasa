73ac32bb9902b5010c3d1b562efeb9429446a8f2
박은우 <eunwoo@bag-eun-uui-MacBookAir.local>
2024-08-16 23:31:40 +0900

    Change IntoPage to Compose UI
    * 부가 수정사항
     - Kotlin 버전 1.8.0 업데이트
     - Java 버전 11 업데이트
     - deprecated된 kotlin-android-extension 제거

--- app/src/main/java/com/eee/www/nugasa/ui/LicenseActivity.kt
+++ app/src/main/java/com/eee/www/nugasa/ui/LicenseActivity.kt
@@ -1,20 +1,20 @@
 package com.eee.www.nugasa.ui
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.eee.www.nugasa.R
-import kotlinx.android.synthetic.main.activity_license.*
 import java.io.IOException
 import java.io.InputStream
 
 
 class LicenseActivity: AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_license)
 
-        license_textView.text = readLicenseTxt()
+        val licenseTextView = findViewById<TextView>(R.id.license_textView)
+        licenseTextView.text = readLicenseTxt()
     }
 
     private fun readLicenseTxt(): String {
